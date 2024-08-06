4ce557f670fe181275362678ac0d5f87a415919a
minbo <smb5174@naver.com>
2024-08-06 18:10:53 +0900

    feat: 프로젝트 설정

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  sassOptions: {
+    includePaths: ['styles'],
+    additionalData: `@import "src/styles/globals.scss";`,
+  },
+};
+
+export default nextConfig;
---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import ReactQueryProvider from '@/components/common/Provider/ReactQueryProvider';
+
+import '@/styles/_reset.scss';
+
+export const metadata = {
+  title: 'mayo',
+  description: '마요 사이트',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>
+        <ReactQueryProvider>
+          <div id="modal-root"></div>
+          {children}
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
