0b5e4918c60dd0d9ba6523ea9a1c9511b93e33a7
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-08-07 22:27:35 +0900

    feat: 마이페이지 버튼추가

--- src/app/my-page/page.jsx
+++ src/app/my-page/page.jsx
@@ -1,3 +1,12 @@
+import styles from '@/app/my-page/page.module.scss';
+
 export default function MainPage() {
-  return <div>마이페이지</div>;
+  return <div className={styles.buttonGrid}>
+    <button>상품 정보</button>
+    <button>알림 설정</button>
+    <button>가게 정보</button>
+    <button>고객 센터</button>
+    <button>마요 공지사항</button>
+    <button>약관 및 정책</button>
+  </div>;
 }
