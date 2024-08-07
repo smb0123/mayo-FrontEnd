94ab68c80e7413b56d8c4e79e2dfa402ad8a8287
minbo <smb5174@naver.com>
2024-08-07 17:39:54 +0900

    feat: 헤더 컴포넌트 구현

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,57 @@
+'use client';
+
+import classNames from 'classnames/bind';
+
+import styles from '@/components/common/Header/Header.module.scss';
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import getStoreInfo from '@/components/common/Header/apis/getStoreInfo';
+
+const cn = classNames.bind(styles);
+
+export default function Header() {
+  const [currentDateTime, setCurrentDateTime] = useState('');
+
+  const storeId = 'VQtTGTCc13EWulU5sZmI';
+
+  const { data, isFetching } = useQuery({
+    queryKey: ['storeStatus'],
+    queryFn: () => getStoreInfo(storeId),
+  });
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      const options = {
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+        timeZone: 'Asia/Seoul',
+        weekday: 'short',
+      };
+
+      const formattedDateTime = now
+        .toLocaleString('ko-KR', options)
+        .replace(/(\d{2})\/(\d{2})/, '$2.$1')
+        .replace(' ', ' ');
+      setCurrentDateTime(formattedDateTime);
+    };
+
+    updateDateTime();
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <header className={cn('container')}>
+      <div className={cn('businessStatusBox')}>
+        <div className={cn('businessStatus', { open: data?.openState })}></div>
+        <p>{data?.openState ? '영업중' : '영업전'}</p>
+      </div>
+      <p className={cn('date')}>{currentDateTime}</p>
+    </header>
+  );
+}
