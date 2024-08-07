41ee2a9a4171eeddb5611a34f7bd1a1c44f8eb25
minbo <smb5174@naver.com>
2024-08-07 16:43:51 +0900

    feat: Layout 컴포넌트 구현

---
+++ src/components/common/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind';
+
+import styles from '@/components/common/Layout/Layout.module.scss';
+import Header from '@/components/common/Header/Header';
+import SideBar from '@/components/common/SideBar/SideBar';
+
+const cn = classNames.bind(styles);
+
+export default function Layout({ children }) {
+  return (
+    <div className={cn('container')}>
+      <SideBar />
+      <div className={cn('box')}>
+        <Header />
+        {children}
+      </div>
+    </div>
+  );
+}
