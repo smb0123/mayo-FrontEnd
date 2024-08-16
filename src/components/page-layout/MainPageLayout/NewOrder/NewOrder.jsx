d887fd926c1e674549451ac97556debbccc03701
minbo <smb5174@naver.com>
2024-08-16 21:57:41 +0900

    feat: NewOrder 컴포넌트 구현

---
+++ src/components/page-layout/MainPageLayout/NewOrder/NewOrder.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import classNames from 'classnames/bind';
+
+import styles from '@/components/page-layout/MainPageLayout/NewOrder/NewOrder.module.scss';
+import Order from '@/components/common/Order/Order';
+import getNewOrder from '@/components/common/Order/api/getNewOrder';
+import { useQuery } from '@tanstack/react-query';
+
+const cn = classNames.bind(styles);
+
+export default function NewOrder() {
+  const test = 'VQtTGTCc13EWulU5sZmI';
+  const { data } = useQuery({
+    queryKey: ['newOrder'],
+    queryFn: () => getNewOrder(test),
+  });
+
+  return (
+    <div className={cn('container')}>
+      <header className={cn('header')}>신규 {data?.length}건</header>
+      <div className={cn('newDetailOrderBox')}>
+        {data?.length > 0 ? (
+          data.map((order, id) => (
+            <Order
+              key={id}
+              menu={order.firstItemName}
+              date={order.createdAt.seconds}
+              id={order.reservationId}
+              orderStatus="new"
+            />
+          ))
+        ) : (
+          <p className={cn('noNewOrder')}>신규주문건이 없습니다</p>
+        )}
+      </div>
+    </div>
+  );
+}
