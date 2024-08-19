15020a9e71ca4016dfee6bccb18605de48be36b9
minbo <smb5174@naver.com>
2024-08-19 14:43:08 +0900

    refactor: 완료 페이지에서 주문 상세 목데이터 제거 후 api 데이터로 변경, 주문 상태에 따라 메시지 변경

--- src/components/page-layout/CompletePageLayout/CompleteDetailOrder/CompleteDetailOrder.jsx
+++ src/components/page-layout/CompletePageLayout/CompleteDetailOrder/CompleteDetailOrder.jsx
@@ -9,24 +9,6 @@ import { CompleteOrderContext } from '../CompletePageLayout';
 
 const cn = classNames.bind(styles);
 
-const data1 = {
-  createdAt: {
-    seconds: 1723376049,
-  },
-  itemCount: [1, 2],
-  itemName: ['바삭바삭 로투스 요거팡 2인', '허파'],
-  pickupTime: {
-    seconds: 1723376400,
-  },
-  request: '소스빼주세요',
-  reservationId: 'id',
-  reservationIsPlastic: true,
-  subTotal: [7700, 5000],
-  totalPrice: 7700,
-  totalQuantity: 1,
-  userNickName: '민보',
-};
-
 export default function CompleteDetailOrder() {
   const { orderId } = useContext(CompleteOrderContext);
 
@@ -36,14 +18,14 @@ export default function CompleteDetailOrder() {
     enabled: !!orderId,
   });
 
-  const menuList = data1?.itemCount.map((count, index) => ({
+  const menuList = data?.itemCount.map((count, index) => ({
     count: count,
-    subTotal: data1.subTotal[index],
-    itemName: data1.itemName[index],
+    subTotal: data?.subTotal[index],
+    itemName: data?.itemName[index],
   }));
 
-  const createTime = new Date(data1.createdAt.seconds * 1000);
-  const pickUpTime = new Date(data1.pickupTime.seconds * 1000);
+  const createTime = new Date(data?.createdAt.seconds * 1000);
+  const pickUpTime = new Date(data?.pickupTime.seconds * 1000);
 
   return (
     <>
@@ -51,31 +33,31 @@ export default function CompleteDetailOrder() {
         <div className={cn('container')}>
           <div className={cn('titleBox')}>
             <p className={cn('menuTitle')}>
-              메뉴 {data1?.totalQuantity}개 총 {data1?.totalPrice.toLocaleString()}원
+              메뉴 {data?.menuTypeCount}개 총 {data?.totalPrice.toLocaleString()}원
             </p>
           </div>
           <div className={cn('detailInfoBox')}>
             <div className={cn('leftDetailInfoBox')}>
               <div className={cn('requestBox')}>
                 <p className={cn('requestTitle')}>요청사항</p>
-                <p className={cn('request')}>{data1.request}</p>
+                <p className={cn('request')}>{data?.request}</p>
                 <p className={cn('request')}>
-                  일회용품 사용 여부: {data1.reservationIsPlastic ? '필요해요 !' : '필요없어요 !'}
+                  일회용품 사용 여부: {data?.reservationIsPlastic ? '필요해요 !' : '필요없어요 !'}
                 </p>
               </div>
               <div className={cn('detailOrderBox')}>
                 <p className={cn('detailOrderTitle')}>주문내역</p>
                 <div className={cn('detailOrderContentBox')}>
                   <div className={cn('detailOrderContent')}>
-                    {menuList.map((menu, idx) => (
+                    {menuList?.map((menu, idx) => (
                       <Menu name={menu.itemName} number={menu.count} price={menu.subTotal} key={idx} />
                     ))}
                   </div>
                   <div className={cn('hr')}></div>
                   <div className={cn('totalBox')}>
                     <div className={cn('totalDetailBox')}>
-                      <p>{data1.totalQuantity}</p>
-                      <p>{data1.totalPrice.toLocaleString()}원</p>
+                      <p>{data?.totalQuantity}</p>
+                      <p>{data?.totalPrice.toLocaleString()}원</p>
                     </div>
                   </div>
                 </div>
@@ -83,13 +65,14 @@ export default function CompleteDetailOrder() {
             </div>
             <div className={cn('rightDetailInfoBox')}>
               <div className={cn('statusBox')}>
-                <p className={cn('title')}>완료된 주문입니다!</p>
-                <p className={cn('name')}>{data1?.userNickName}(닉네임)</p>
-                <p className={cn('phoneNumber')}>010-0000-0000</p>
+                <p className={cn('title', { fail: data?.reservationState === 3 })}>
+                  {data?.reservationState === 2 ? '완료된 주문입니다!' : '실패된 주문입니다!'}{' '}
+                </p>
+                <p className={cn('name')}>{data?.userNickName}(닉네임)</p>
               </div>
               <div className={cn('dateBox')}>
                 <div className={cn('reservationIdBox')}>
-                  예약번호: <p className={cn('reservationId')}>{data1.reservationId}</p>
+                  예약번호: <p className={cn('reservationId')}>{data?.reservationId}</p>
                 </div>
                 <div className={cn('reservationDateBox')}>
                   예약일시:
