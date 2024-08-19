384b48128cfac5ee166b7c202217b277519f2fa8
minbo <smb5174@naver.com>
2024-08-19 14:44:53 +0900

    feat: CompleteOrder 컴포넌트에서 목데이터 제거 후 api 데이터 연결

--- src/components/page-layout/CompletePageLayout/CompleteOrder/CompleteOrder.jsx
+++ src/components/page-layout/CompletePageLayout/CompleteOrder/CompleteOrder.jsx
@@ -4,41 +4,32 @@ import styles from '@/components/page-layout/CompletePageLayout/CompleteOrder/Co
 import Order from '@/components/common/Order/Order';
 import { useContext } from 'react';
 import { CompleteOrderContext } from '../CompletePageLayout';
+import getDoneOrder from './api/getDoneOrder';
+import { useQuery } from '@tanstack/react-query';
 
 const cn = classNames.bind(styles);
 
-const data = [
-  {
-    firstItemName: '짬떡',
-    createdAt: {
-      seconds: 1723374554,
-    },
-    reservationId: 's',
-  },
-  {
-    firstItemName: '짬떡',
-    createdAt: {
-      seconds: 1723374554,
-    },
-    reservationId: 's',
-  },
-];
-
 export default function CompleteOrder() {
   const { setOrderId } = useContext(CompleteOrderContext);
+  const test = 'VQtTGTCc13EWulU5sZmI';
+
+  const { data } = useQuery({
+    queryKey: ['doneOrder'],
+    queryFn: () => getDoneOrder(test),
+  });
 
   return (
     <div className={cn('container')}>
       <header className={cn('header')}>완료 {data?.length}건</header>
       <div className={cn('completeDetailOrderBox')}>
         {data?.length > 0 ? (
-          data.map((order, idx) => (
+          data?.map((order, idx) => (
             <Order
               key={idx}
               menu={order.firstItemName}
               date={order.createdAt.seconds}
               id={order.reservationId}
-              orderStatus="new"
+              orderStatus="done"
               setOrderId={setOrderId}
               setOrderStatus={false}
             />
