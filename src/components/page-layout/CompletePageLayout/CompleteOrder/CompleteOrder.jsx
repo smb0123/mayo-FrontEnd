95dfb148d8788560fd5879728498a93b53874dda
minbo <smb5174@naver.com>
2024-08-21 16:16:47 +0900

    feat: 무한스크롤 구현

--- src/components/page-layout/CompletePageLayout/CompleteOrder/CompleteOrder.jsx
+++ src/components/page-layout/CompletePageLayout/CompleteOrder/CompleteOrder.jsx
@@ -2,33 +2,48 @@ import classNames from 'classnames/bind';
 
 import styles from '@/components/page-layout/CompletePageLayout/CompleteOrder/CompleteOrder.module.scss';
 import Order from '@/components/common/Order/Order';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CompleteOrderContext } from '../CompletePageLayout';
 import getDoneOrder from './api/getDoneOrder';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
 const cn = classNames.bind(styles);
 
 export default function CompleteOrder() {
   const { setOrderId } = useContext(CompleteOrderContext);
   const test = 'VQtTGTCc13EWulU5sZmI';
+  const date = '2024-08-19T12:00:00Z';
+  const [lastRef, inView] = useInView();
+  const orderArray = [];
 
-  const { data } = useQuery({
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['doneOrder'],
-    queryFn: () => getDoneOrder(test),
+    queryFn: ({ pageParam }) => getDoneOrder(test, date, pageParam, 8),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) =>
+      !lastPage.last ? lastPage.pageable.pageNumber + 1 : undefined,
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  data?.pages.map((order) => order.content.map((detailOrder) => orderArray.push(detailOrder)));
 
   return (
     <div className={cn('container')}>
-      <header className={cn('header')}>완료 {data?.length}건</header>
+      <header className={cn('header')}>완료 {data?.content?.length}건</header>
       <div className={cn('completeDetailOrderBox')}>
-        {data?.length > 0 ? (
-          data?.map((order, idx) => (
+        {orderArray ? (
+          orderArray?.map((order, idx) => (
             <Order
               key={idx}
               menu={order.firstItemName}
               date={order.createdAt.seconds}
-              id={order.reservationId}
+              id={order.id}
               orderStatus="done"
               setOrderId={setOrderId}
               setOrderStatus={false}
@@ -37,6 +52,7 @@ export default function CompleteOrder() {
         ) : (
           <p className={cn('noCompleteOrder')}>완료 건이 없습니다</p>
         )}
+        {data && <div ref={lastRef}></div>}
       </div>
     </div>
   );
--- src/components/page-layout/CompletePageLayout/CompleteOrder/api/getDoneOrder.js
+++ src/components/page-layout/CompletePageLayout/CompleteOrder/api/getDoneOrder.js
@@ -1,6 +1,8 @@
 import axiosInstance from '@/apis/axiosInstance';
 
-export default async function getDoneOrder(id) {
-  const { data } = await axiosInstance.get(`reservation-done-async?storeId=${id}`);
+export default async function getDoneOrder(id, date, page, size) {
+  const { data } = await axiosInstance.get(
+    `reservations-end-slice-time?storeId=${id}&timeStamp=${date}&page=${page}&size=${size}`
+  );
   return data;
 }
