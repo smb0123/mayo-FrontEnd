0de790f0904b5909515f09dc2fff6a2efada1501
minbo <smb5174@naver.com>
2024-09-02 15:36:24 +0900

    refactor: (Layout)으로 폴더 이동

---
+++ src/app/(Layout)/in-progress/page.jsx
@@ -0,0 +1,5 @@
+import InProgressPageLayout from '@/components/page-layout/InProgressPageLayout/InProgressPageLayout';
+
+export default function InProgressPage() {
+  return <InProgressPageLayout />;
+}
