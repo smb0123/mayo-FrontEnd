f45445745acaf35f1ac6a7162ee55606658ab8ae
minbo <smb5174@naver.com>
2024-09-02 15:34:11 +0900

    feat: 기존 layout.jsx 파일 위치 변경

---
+++ src/app/(Layout)/layout.jsx
@@ -0,0 +1,22 @@
+import Layout from '@/components/common/Layout/Layout';
+import ReactQueryProvider from '@/components/common/Provider/ReactQueryProvider';
+
+import '@/styles/_reset.scss';
+
+export const metadata = {
+  title: 'mayo',
+  description: '마요 사이트',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>
+        <ReactQueryProvider>
+          <div id="modal-root"></div>
+          <Layout>{children}</Layout>
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
