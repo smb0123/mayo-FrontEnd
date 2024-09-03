5824f7e2a4cf5b3d959d78bc6dac689e98373c78
minbo <smb5174@naver.com>
2024-09-03 11:57:45 +0900

    refactor: Layout.jsx 파일 수정

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -4,28 +4,17 @@ import classNames from 'classnames/bind';
 import styles from '@/components/common/Layout/Layout.module.scss';
 import Header from '@/components/common/Header/Header';
 import SideBar from '@/components/common/SideBar/SideBar';
-import { usePathname } from 'next/navigation'; 
 
 const cn = classNames.bind(styles);
 
 export default function Layout({ children }) {
-  const pathname = usePathname(); 
-
- 
-  const isLoginPage = pathname === '/login';
-
   return (
-    
-    !isLoginPage ? (
-      <div className={cn('container')}>
-        <SideBar />
-        <div className={cn('box')}>
-          <Header />
-          {children}
-        </div>
+    <div className={cn('container')}>
+      <SideBar />
+      <div className={cn('box')}>
+        <Header />
+        {children}
       </div>
-    ) : (
-      children 
-    )
+    </div>
   );
 }
