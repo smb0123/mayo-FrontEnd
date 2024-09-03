4524b0b04f5d93342f5e4a59412c73df98f3eb26
minbo <smb5174@naver.com>
2024-09-03 23:30:02 +0900

    refactor: 잘못된 api 주소 수정

--- src/components/page-layout/CompletePageLayout/CompleteOrder/api/getDoneOrder.js
+++ src/components/page-layout/CompletePageLayout/CompleteOrder/api/getDoneOrder.js
@@ -1,9 +1,7 @@
 import axiosInstance from '@/apis/axiosInstance';
 
-export default async function getDoneOrder(id, date, page, size) {
+export default async function getDoneOrder(id, date) {
   const formatDate = date + 'T00:00:00';
-  const { data } = await axiosInstance.get(
-    `reservations-end-slice-time?storeId=${id}&timeStamp=${formatDate}&page=${page}&size=${size}`
-  );
+  const { data } = await axiosInstance.get(`reservation-done-time?storeId=${id}&timestamp=${formatDate}`);
   return data;
 }
