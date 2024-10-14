bfbe692f7d0199898cb15faed3287e5ad5d4aead
howon <56529592+howon-Jeon@users.noreply.github.com>
2024-10-14 13:12:01 +0900

    bug/로그인 라우터 수정

--- src/components/page-layout/MainPageLayout/MainPageLayout.jsx
+++ src/components/page-layout/MainPageLayout/MainPageLayout.jsx
@@ -15,9 +15,10 @@ import {
   getRedirectResult,
   setPersistence,
   browserLocalPersistence,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { auth } from '@/firebase';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { useState, useEffect, useCallback } from 'react';
 import axiosInstance from '@/apis/axiosInstance';
 import ROUTE from '@/constants/route';
@@ -29,6 +30,7 @@ export default function MainPageLayout() {
   const { setStoreId } = useStoreId();
   const { setUserId } = useUserId();
   const router = useRouter();
+  const pathname = usePathname();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -75,6 +77,18 @@ export default function MainPageLayout() {
 
     checkRedirectResult();
   }, [checkUserPermissions]);
+
+  useEffect(() => {
+    // 로그인 상태 변화 감지 및 보호된 페이지 접근 제어
+    const loginstate = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        if (pathname !== ROUTE.HOME) {
+          router.push(ROUTE.HOME);
+        }
+      }
+    });
+    return () => loginstate();
+  }, [router, pathname]);
 
   const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider();
