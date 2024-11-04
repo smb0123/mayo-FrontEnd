54fa8176014917d7121f73548570b181d41d0e16
minbo <smb5174@naver.com>
2024-11-04 11:24:26 +0900

    refactor: 신규 주문 요청 api 주소 수정

--- src/components/common/Order/api/getNewOrder.js
+++ src/components/common/Order/api/getNewOrder.js
@@ -1,6 +1,6 @@
 import axiosInstance from '@/apis/axiosInstance';
 
 export default async function getNewOrder(id) {
-  const { data } = await axiosInstance.get(`reservation-new-async?storeId=${id}`);
+  const { data } = await axiosInstance.get(`reservation-new?storeId=${id}`);
   return data;
 }
