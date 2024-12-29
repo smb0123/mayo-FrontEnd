01702db3d3d16c6d1c8946de8b9a1b53a3238342
minbo <smb5174@naver.com>
2024-12-29 11:38:59 +0900

    refactor: 인터셉터 변경

--- src/apis/multiPartAxiosInstance.ts
+++ src/apis/multiPartAxiosInstance.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { auth } from '@/firebase'; 
 
 const multiPartAxiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
@@ -9,15 +8,12 @@ const multiPartAxiosInstance = axios.create({
   },
 });
 
-// 요청 인터셉터를 사용하여 모든 요청에 토큰을 추가합니다.
 multiPartAxiosInstance.interceptors.request.use(
   async (config) => {
     try {
-      // 현재 로그인된 사용자의 ID 토큰을 가져옵니다.
-      const token = await auth.currentUser?.getIdToken();
-      console.log('ID Token:', token); // 토큰을 콘솔에 출력하여 확인
+      const token = localStorage.getItem('token');
       if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // Authorization 헤더에 토큰을 추가합니다.
+        config.headers.Authorization = `Bearer ${token}`;
       }
     } catch (error) {
       console.error('토큰을 가져오는 중 오류 발생:', error);
