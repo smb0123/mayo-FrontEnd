263bdd57f5b41b7b45cdffd4cd7b2de9d50635c9
smb0123 <smb5174@naver.com>
2025-03-16 05:00:32 +0000

    refactor: 메인 페이지 SSR 제거

--- src/app/(NoLayout)/page.jsx
+++ src/app/(NoLayout)/page.jsx
@@ -1,4 +1,9 @@
-import MainPageLayout from '@/components/page-layout/MainPageLayout/MainPageLayout';
+import dynamic from 'next/dynamic';
+
+const MainPageLayout = dynamic(() => import('@/components/page-layout/MainPageLayout/MainPageLayout'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 export default function MainPage() {
   return <MainPageLayout />;
